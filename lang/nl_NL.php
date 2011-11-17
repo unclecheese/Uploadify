6dbd66d907ecdbbe2c558d67eae3f7de11821d93
Simon Erkelens <simon@simon-klopbox.(none)>
2011-11-17 16:22:50 +0100

    MINOR: Dutch langfile

---
+++ lang/nl_NL.php
@@ -0,0 +1,24 @@
+<?php
+
+global $lang;
+
+$lang['nl_NL']['Uploadify']['ATTACHEDFILES'] = 'Gekoppelde bestanden';
+$lang['nl_NL']['Uploadify']['BUTTONTEXT'] = 'Bestand zoeken';
+$lang['nl_NL']['Uploadify']['CHANGEFOLDERACTION'] = 'Bewerken';
+$lang['nl_NL']['Uploadify']['CHOOSEEXISTING'] = 'Kies bestaande';
+$lang['nl_NL']['Uploadify']['CHOOSEIMPORTFOLDER'] = 'Kies een map';
+$lang['nl_NL']['Uploadify']['CHOOSEUPLOADFOLDER'] = 'Kies een map om afbeeldingen in te importeren';
+$lang['nl_NL']['Uploadify']['DOIMPORT'] = 'Importeer';
+$lang['nl_NL']['Uploadify']['IMAGES'] = 'Afbeeldingen';
+$lang['nl_NL']['Uploadify']['NOFILE'] = 'Geen bestand in de bijlage';
+$lang['nl_NL']['Uploadify']['NOFILES'] = 'Geen bestanden in de bijlage';
+$lang['nl_NL']['Uploadify']['NOFILESFOUND'] = 'Er zijn geen bestanden in deze map';
+$lang['nl_NL']['Uploadify']['PLEASESELECT'] = 'Selecteer een map';
+$lang['nl_NL']['Uploadify']['REMOVE'] = 'Ontkoppel';
+$lang['nl_NL']['Uploadify']['REMOVEANDDELETE'] = 'Ontkoppel en verwijder permanent';
+$lang['nl_NL']['Uploadify']['SUCCESSFULADDMULTI'] = 'Bestanden zijn met succes toegevoegd';
+$lang['nl_NL']['Uploadify']['SUCCESSFULADDSINGLE'] = 'Bestand \"%s\" is met succes toegevoegd.';
+$lang['nl_NL']['Uploadify']['UPLOADNEW'] = 'Nieuwe afbeeldingen uploaden';
+$lang['nl_NL']['Uploadify']['PLEASESELECT'] = 'Selecteer';
+$lang['nl_NL']['Uploadify']['DETATCH'] = 'Ontkoppel';
+$lang['nl_NL']['Uploadify']['DELETEPERMANENTLY'] = 'Verwijder permanent';
