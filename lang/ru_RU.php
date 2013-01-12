6ac415715a5ef3b2183c9c9b2670711251cd30de
unknown <C:\Users\Eugene\AppData\Roaming\The Bat! Pwd>
2013-01-12 12:34:45 +0200

    Russian and Ukrainian localization.

---
+++ lang/ru_RU.php
@@ -0,0 +1,27 @@
+<?php
+global $lang;
+
+$lang['ru_RU']['Uploadify']['SUCCESSFULADDSINGLE'] = 'Файл "%s" успешно добавлен.';
+$lang['ru_RU']['Uploadify']['SUCCESSFULADDMULTI'] = 'Файлы успешно добавлены.';
+$lang['ru_RU']['Uploadify']['BUTTONTEXT'] = 'Выбeрите&nbsp;файл';
+$lang['ru_RU']['Uploadify']['CHANGEFOLDERACTION'] = 'сменить';
+$lang['ru_RU']['Uploadify']['CHOOSEIMPORTFOLDER'] = 'Выбрать каталог';
+$lang['ru_RU']['Uploadify']['PLEASESELECT'] = 'Выберите каталог';
+$lang['ru_RU']['Uploadify']['IMAGES'] = 'Изображения';
+$lang['ru_RU']['Uploadify']['ATTACHEDFILES'] = 'Прикреплённые файлы';
+$lang['ru_RU']['Uploadify']['REMOVE'] = 'Убрать связь';
+$lang['ru_RU']['Uploadify']['REMOVEANDDELETE'] = 'Убрать связь и удалить';
+$lang['ru_RU']['Uploadify']['NOFILES'] = 'Нет прикреплённых файлов';
+$lang['ru_RU']['Uploadify']['NOFILE'] = 'Нет прикреплённых файлов';
+$lang['ru_RU']['Uploadify']['CHOOSEUPLOADFOLDER'] = 'Выберите каталог для загрузки';
+$lang['ru_RU']['Uploadify']['NOFILESFOUND'] = 'В этом каталоге нет файлов';
+$lang['ru_RU']['Uploadify']['CHOOSEEXISTING'] = 'Выбрать существующий';
+$lang['ru_RU']['Uploadify']['UPLOADNEW'] = 'Загрузить';
+$lang['ru_RU']['Uploadify']['DOIMPORT'] = 'Импортировать';
+$lang['ru_RU']['Uploadify']['DETATCH'] = 'отсоединить';
+$lang['ru_RU']['Uploadify']['DELETEPERMANENTLY'] = 'удалить без возможности восстановления';
+$lang['ru_RU']['Uploadify']['SELECT'] = 'Отметить';
+$lang['ru_RU']['Uploadify']['ALL'] = 'все';
+$lang['ru_RU']['Uploadify']['NONE'] = 'ни одного';
+$lang['ru_RU']['Uploadify']['TOGGLE'] = 'инвертировать';
+$lang['ru_RU']['Uploadify']['FILETYPENOTALLOWED'] = 'Файлы с таки расширением не принимаются!';
